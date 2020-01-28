e55f9a373fbacd64843730fbc308e9844d65174b
gboyega <jaiyeolagboyega@gmail.com>
2020-01-28 18:16:44 +0100

    created file structure and server program

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const path = require("path");
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console,'error: '));
+db.once('open', () => {
+    console.log('MongoDB database connection established!');
+});
+
+app.listen(process.env.PORT || 4000, ()=> console.log('APP listening on port 4000'));
