959c8b039435896e5e5616031820caa30bc6c68a
gboyega <jaiyeolagboyega@gmail.com>
2020-01-30 04:13:27 +0100

    added create to-do form

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,81 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
-  useEffect (() => {
+  const [title, setTitle] = useState("");
+
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
     try {
-      fetch('/api/todos')
-      .then(response => response.json())
-      .then(data => setTodos(data.body));
+      fetch("/api/todos")
+        .then(response => response.json())
+        .then(data => setTodos(data.body));
     } catch (error) {
       console.log(error);
     }
   }, []);
-  
+
+  const formSubmit = e => {
+    e.preventDefault();
+    
+    try {
+      fetch("/api/todos", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({title, content})
+      })
+        .then(response => response.json())
+        .then(data => alert('Todo Created.'));
+    } catch (error) {
+      console.log(error);
+    } 
+  };
+
   return (
     <div className="App">
-     <h1>ToDo App</h1>
+      <h1>ToDo App</h1>
 
-     {/* list todos */}
-     <div>
-       {todos.map((todo, i) => {
-         return (
-           <div key={i}>
-             <h4>{todo.title}</h4>
+      {/* list todos */}
+      <div>
+        {todos.map((todo, i) => {
+          return (
+            <div key={i}>
+              <h4>{todo.title}</h4>
 
-             <p>{todo.content}</p>
+              <p>{todo.content}</p>
 
-             <button value>Delete</button>
+              <button value>Delete</button>
 
-             <button>Edit</button>
-           </div>
-         );
-       }
-         
-       )}
-     </div>
+              <button>Edit</button>
+            </div>
+          );
+        })}
+      </div>
 
-     {/* create todos */}
+      {/* create todos */}
+      <div>
+        <form onSubmit={formSubmit}>
+          <label>Title</label>
+          <input
+            name="title"
+            required
+            onChange={e => setTitle(e.target.value)}
+          />
 
-     {/* update todos */}
+          <label>Content</label>
+          <input
+            name="content"
+            required
+            onChange={e => setContent(e.target.value)}
+          />
+
+          <input type="submit" />
+        </form>
+      </div>
+
+      {/* update todos */}
     </div>
   );
 }
